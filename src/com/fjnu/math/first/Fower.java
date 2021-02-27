1cf7615d811a0e92d53cb672fcec245eaec4506e
Huangshujian <32355963+Huangshujian@users.noreply.github.com>
2021-02-27 16:22:50 +0800

    Update Fower.java

--- src/com/fjnu/math/first/Fower.java
+++ src/com/fjnu/math/first/Fower.java
@@ -3,6 +3,7 @@ package com.fjnu.math.first;
 public class Fower {
 	public static void main(String [] args) {
 		int a,b,c,dd;
+		int aaaa;
 		for(int i=100;i<=999;i++) {
 			a=i/100;
 			b=(i/10)%10;
